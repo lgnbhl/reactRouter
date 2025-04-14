383ad565cbf481c49bc625921f25b5a471b36e03
lgnbhl <felix.luginbuhl@protonmail.ch>
2025-04-14 20:17:51 +0200

    remove wrong ButtonAdapter and update react-router-dom JS file accordingly

--- js/src/inputs.jsx
+++ js/src/inputs.jsx
@@ -3,4 +3,3 @@ import { ButtonAdapter } from '@/shiny.react';
 
 export const Link = ButtonAdapter(ReactRouter.Link);
 export const NavLink = ButtonAdapter(ReactRouter.NavLink);
-export const Route = ButtonAdapter(ReactRouter.Route);
