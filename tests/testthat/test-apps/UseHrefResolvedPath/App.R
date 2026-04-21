62db6b07a53c4b84176353ae1bd297d6ba9c1145
lgnbhl <felix.luginbuhl@protonmail.ch>
2026-04-21 18:17:39 +0200

    fix test useResolvedPath

--- tests/testthat/test-apps/UseHrefResolvedPath/App.R
+++ tests/testthat/test-apps/UseHrefResolvedPath/App.R
@@ -2,41 +2,39 @@ library(shiny)
 
 ui <- reactRouter::RouterProvider(
   router = reactRouter::createHashRouter(
-    reactRouter::Routes(
+    reactRouter::Route(
+      path = "/",
+      element = div(reactRouter::Outlet()),
       reactRouter::Route(
-        path = "/",
-        element = div(reactRouter::Outlet()),
-        reactRouter::Route(
-          path = "products/:id",
-          element = div(
-            div(
-              id = "hrefRelative",
-              reactRouter::useHref(
-                tags$span(),
-                to = "../settings"
-              )
-            ),
-            div(
-              id = "hrefAbsolute",
-              reactRouter::useHref(
-                tags$span(),
-                to = "/home"
-              )
-            ),
-            div(
-              id = "resolvedAll",
-              reactRouter::useResolvedPath(
-                tags$span(),
-                to = "../settings"
-              )
-            ),
-            div(
-              id = "resolvedPathname",
-              reactRouter::useResolvedPath(
-                tags$span(),
-                to = "../settings",
-                selector = "pathname"
-              )
+        path = "products/:id",
+        element = div(
+          div(
+            id = "hrefRelative",
+            reactRouter::useHref(
+              tags$span(),
+              to = "../settings"
+            )
+          ),
+          div(
+            id = "hrefAbsolute",
+            reactRouter::useHref(
+              tags$span(),
+              to = "/home"
+            )
+          ),
+          div(
+            id = "resolvedAll",
+            reactRouter::useResolvedPath(
+              tags$span(),
+              to = "../settings"
+            )
+          ),
+          div(
+            id = "resolvedPathname",
+            reactRouter::useResolvedPath(
+              tags$span(),
+              to = "../settings",
+              selector = "pathname"
             )
           )
         )
