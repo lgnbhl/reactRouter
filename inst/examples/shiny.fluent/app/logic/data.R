95bd91aa056129be28e6744253422cabec640201
lgnbhl <felix.luginbuhl@protonmail.ch>
2026-05-10 10:20:39 +0200

    fix bug in "shiny.fluent" example

--- inst/examples/shiny.fluent/app/logic/data.R
+++ inst/examples/shiny.fluent/app/logic/data.R
@@ -2,5 +2,8 @@
 
 print("Fetching hero stats from OpenDota API...")
 hero_stats <- httr::GET("https://api.opendota.com/api/heroStats")
-hero_stats <- httr::content(hero_stats)
+hero_stats <- jsonlite::fromJSON(
+    httr::content(hero_stats, as = "text", encoding = "UTF-8"),
+    simplifyVector = FALSE
+)
 hero_stats
